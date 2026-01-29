1fbdbabd9615ae84b3ce1e00c824d4f7a4258bb3
goutham-rap <Goutham.s@rap.ventures>
2026-01-29 12:26:53 +0530

    Updates

---
+++ _next/static/chunks/7f5df0b5006ae244.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/d139b46e8ae252d2.js",
+  "static/chunks/turbopack-d52302e268a8455a.js"
+])
---
+++ _next/static/chunks/e5dd8137af18e9b1.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/d139b46e8ae252d2.js",
+  "static/chunks/turbopack-d5dc28501a1d21f3.js"
+])
